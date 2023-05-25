33f6969b1d69d5996657cb741c73a4c93cac413c
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-05-25 17:27:41 +0530

    Add files via upload

---
+++ Task 2(Quiz Application)/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+      {
+        numb: 1,
+        question: "36, 34, 30, 28,___, 22 What number should come to fill in the blank space?",
+        answer: "24",
+        options: [
+          "24",
+          "26",
+          "25",
+          "None of the above"
+        ]
+      },
+      {
+        numb: 2,
+        question: "What is the largest prime palindrome(Roman Numbers)?",
+        answer: "19",
+        options: [
+          "1",
+          "5",
+          "71",
+          "19"
+        ]
+      },
+      {
+        numb: 3,
+        question: "n! has n digits.What is n?(n is an integer)",
+        answer: "22",
+        options: [
+          "22",
+          "21",
+          "23",
+          "24"
+        ]
+      },
+      {
+        numb: 4,
+        question: "Which is the only number that is spelt with letters arranged in alphabetical order?",
+        answer: "40",
+        options: [
+          "30",
+          "20",
+          "1",
+          "40"
+        ]
+      },
+      {
+        numb: 5,
+        question: "str(n*n)+str(n*n*n) has all the digits(0 to 9).What is n?",
+        answer: "69",
+        options: [
+          "69",
+          "73",
+          "23",
+          "29"
+        ]
+      },
+      {
+        numb: 6,
+        question: "Which is the only number that is spelt with letters arranged in alphabetical(reverse) order?",
+        answer: "1",
+        options: [
+          "30",
+          "20",
+          "1",
+          "40"
+        ]
+      },
+      {
+        numb: 7,
+        question: "Which is the first number that has 'a' in it's spelling?",
+        answer: "1000",
+        options: [
+          "10000",
+          "100",
+          "1000",
+          "10000"
+        ]
+      },
+      {
+        numb: 8,
+        question: "The integer n has n letters in it's spelling?",
+        answer: "4",
+        options: [
+          "5",
+          "7",
+          "4",
+          "11"
+        ]
+      },
+      {
+        numb: 9,
+        question: "Every ______ number has 'e' in it's spelling?",
+        answer: "odd",
+        options: [
+          "even",
+          "prime",
+          "composite",
+          "odd"
+        ]
+      },
+      {
+        numb: 10,
+        question: "The only number that is not represented by the Roman numerals?",
+        answer: "0",
+        options: [
+          "0",
+          "1000000",
+          "100000000",
+          "10000000000"
+        ]
+      },
+    ];
