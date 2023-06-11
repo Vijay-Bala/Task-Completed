8e0a32b78e2e0feac9a3f320181baafffe7ed9ac
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-06-11 18:54:05 +0530

    Add files via upload

---
+++ Task 3(Music Player)/js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+
+        {
+            name: "Home",
+            artist: "Jordan Scho",
+            img: "music-1",
+            src: "music-1"
+        },
+        {
+            name: "Ikson Anywhere",
+            artist: "Audio Library",
+            img: "music-2",
+            src: "music-2"
+        },
+        {
+            name: "Crazy",
+            artist: "Beauz & Jvna",
+            img: "music-3",
+            src: "music-3"
+        },
+        {
+            name: "Want Me",
+            artist: "Mike Archangelo",
+            img: "music-4",
+            src: "music-4"
+        },
+        {
+            name: "Sun Goes Down",
+            artist: "Roy X Jim Yosef",
+            img: "music-5",
+            src: "music-5"
+        },
+        {
+            name: "Lost Sky",
+            artist: "NCS Release",
+            img: "music-6",
+            src: "music-6"
+        },
+        {
+            name: "A Thousand Years",
+            artist: "Christina Perri",
+            img: "music-7",
+            src: "music-7"
+        },
+        ];
